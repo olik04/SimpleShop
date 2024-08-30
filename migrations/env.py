18ded9df6e6187478032e5d4af217304a433dd2a
Alikhan Mukashev <alikhan.mukashev@gmail.com>
2024-08-30 17:00:19 +0800

    change default host in config.py

--- migrations/env.py
+++ migrations/env.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 
 from src.core.models import Base
-from src.core import settings
+from src.core import settings 
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
--- src/core/config.py
+++ src/core/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class RunConfig(BaseModel):
-    host: str = "0.0.0.0"
+    host: str = "localhost"
     port: int = 8000
 
 
