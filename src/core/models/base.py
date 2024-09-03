e4d2028c45b2d52b26f6e37eac2debfa5409b4e0
Alikhan Mukashev <alikhan.mukashev@gmail.com>
2024-09-03 20:10:23 +0800

    Update base.py (add id)

--- src/core/models/base.py
+++ src/core/models/base.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, declared_attr
+from sqlalchemy.orm import DeclarativeBase, Mapped, declared_attr, mapped_column
 
 from utils import camel_case_to_snake_case
 
@@ -9,3 +9,5 @@ class Base(DeclarativeBase):
     @declared_attr.directive
     def __tablename__(cls) -> str:
         return camel_case_to_snake_case(cls.__name__)
+
+    id: Mapped[int] = mapped_column(primary_key=True)
