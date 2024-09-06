b29b66d56451f0fed410eadcc28ff2f2872e5bcb
Alikhan Mukashev <alikhan.mukashev@gmail.com>
2024-09-06 22:07:54 +0800

    Update path to absolute

--- src/core/models/user.py
+++ src/core/models/user.py
@@ -2,7 +2,7 @@ from typing import TYPE_CHECKING
 
 from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 
-from . import Base
+from core.models import Base
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
